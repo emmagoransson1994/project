a3f90373f6145082beac48a5e61396defe8ae663
Emma Göransson <emmagoransson@Emmas-MacBook-Pro-2.local>
2020-01-19 15:34:25 +0100

    Updates

---
+++ precache-manifest.e5884a7966d4be71bbd6e4e1855340d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f92f333dcf8dfa9f42a62f51e44b839",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "74be3606661c7417c058",
+    "url": "/project/static/css/2.2cc85d99.chunk.css"
+  },
+  {
+    "revision": "e7d5e95b1ba40dde5dcc",
+    "url": "/project/static/css/main.ee3d5d84.chunk.css"
+  },
+  {
+    "revision": "74be3606661c7417c058",
+    "url": "/project/static/js/2.07a31787.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project/static/js/2.07a31787.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7d5e95b1ba40dde5dcc",
+    "url": "/project/static/js/main.860901cf.chunk.js"
+  },
+  {
+    "revision": "584bfb3bc2caeb97bb0d",
+    "url": "/project/static/js/runtime-main.34264056.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/project/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/project/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/project/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/project/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/project/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
